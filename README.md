2d5b63685497b27fdcf5e79b761bdc7f9e5848c6
Tony Li <394287122@qq.com>
2021-02-07 00:11:47 +0800

    拍照DEMO

--- README.md
+++ README.md
@@ -289,6 +289,69 @@ pubspec.yaml 文件中配置资源目录
 
 > 局部使用字体 style => TextStyle => fontFamily
 
+### 13、拍照DEMO
+
+image_picker 调用相机、相册插件
+
+> 配置 Android、ios 参数，才可使用
+
+```dart
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
+      body: Center(
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
+```
 
 
 
--- lib/flutter_knowledge/photo_app_page.dart
+++ lib/flutter_knowledge/photo_app_page.dart
@@ -1,5 +1,6 @@
-// 拍照app开发
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PhotoApp extends StatefulWidget {
   @override
@@ -7,8 +8,99 @@ class PhotoApp extends StatefulWidget {
 }
 
 class _PhotoAppState extends State<PhotoApp> {
+  List<File> _images = [];
+  final picker = ImagePicker();
+
+  Future getImage(bool isTakePhoto) async {
+    Navigator.pop(context);
+    final pickedFile = await picker.getImage(
+        source: isTakePhoto ? ImageSource.camera : ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _images.add(File(pickedFile.path));
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('拍照DEMO'),
+      ),
+      body: Center(
+        // child: _image == null ? Text('No image selected.') : Image.file(_image),
+        child: Wrap(spacing: 5, runSpacing: 5, children: _genImages()),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pickImage,
+        tooltip: '选择图片',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+
+  _pickImage() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => Container(
+              height: 160,
+              child: Column(
+                children: <Widget>[
+                  _item('拍照', true),
+                  _item('从相册选择', false),
+                ],
+              ),
+            ));
+  }
+
+  _item(String title, bool isTakePhoto) {
+    return GestureDetector(
+      child: ListTile(
+        leading: Icon(isTakePhoto ? Icons.camera_alt : Icons.photo_library),
+        title: Text(title),
+        onTap: () => getImage(isTakePhoto),
+      ),
+    );
+  }
+
+  _genImages() {
+    return _images.map((file) {
+      return Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Image.file(
+              file,
+              width: 120,
+              height: 90,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Positioned(
+            right: 5,
+            top: 5,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _images.remove(file);
+                });
+              },
+              child: ClipOval(
+                // 圆角删除按钮
+                child: Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(color: Colors.black54),
+                  child: Icon(Icons.close, size: 18, color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+    }).toList();
   }
 }
