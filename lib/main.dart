4efeaa238eeea42611367a3af8eaaa7e0a72e980
Tony Li <394287122@qq.com>
2021-03-02 23:16:06 +0800

    app首页框架

--- lib/main.dart
+++ lib/main.dart
@@ -1,184 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart' show timeDilation;
-import 'dart:math' as math;
 
-/// 需要用 MaterialApp 包裹
-void main() => runApp(MaterialApp(home: RadialExpansionDemo()));
-
-class Photo extends StatelessWidget {
-  const Photo({Key key, this.photo, this.onTap, this.width}) : super(key: key);
-  final String photo;
-  final VoidCallback onTap;
-  final double width;
-
+class TabbedAppBarSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).primaryColor.withOpacity(0.25),
-      // 按钮
-      child: InkWell(
-        onTap: onTap,
-        child: LayoutBuilder(
-          builder: (context, size) {
-            return Image.network(
-              photo,
-              fit: BoxFit.contain,
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class RadialExpansion extends StatelessWidget {
-  RadialExpansion({
-    Key key,
-    this.maxRadius,
-    this.child,
-  })  : clipRectSize = 2.0 * (maxRadius / math.sqrt2),
-        super(key: key);
-
-  final double maxRadius;
-  final clipRectSize;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipOval(
-      child: Center(
-        child: SizedBox(
-          width: clipRectSize,
-          height: clipRectSize,
-          child: ClipRect(
-            child: child,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tabbed AppBar'),
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class RadialExpansionDemo extends StatelessWidget {
-  static const double kMinRadius = 32.0;
-  static const double kMaxRadius = 128.0;
-  static const opacityCurve =
-      const Interval(0.0, 0.75, curve: Curves.fastLinearToSlowEaseIn);
-
-  static RectTween _createRectTween(Rect begin, Rect end) {
-    // 方形到圆形变化的辅助类
-    return MaterialRectCenterArcTween(begin: begin, end: end);
-  }
-
-  static Widget _buildPage(
-      BuildContext context, String imageName, String description) {
-    return Container(
-      color: Theme.of(context).canvasColor,
-      child: Center(
-        child: Card(
-          elevation: 8,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              SizedBox(
-                width: kMaxRadius * 2,
-                height: kMaxRadius * 2,
-                child: Hero(
-                  createRectTween: _createRectTween,
-                  tag: imageName,
-                  child: RadialExpansion(
-                    maxRadius: kMaxRadius,
-                    child: Photo(
-                      photo: imageName,
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              Text(
-                description,
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textScaleFactor: 3.0,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildHero(
-      BuildContext context, String imageName, String description) {
-    return Container(
-      width: kMinRadius * 2,
-      height: kMinRadius * 2,
-      child: Hero(
-        createRectTween: _createRectTween,
-        tag: imageName,
-        child: RadialExpansion(
-          maxRadius: kMaxRadius,
-          child: Photo(
-            photo: imageName,
-            onTap: () {
-              Navigator.of(context).push(
-                PageRouteBuilder<void>(
-                  pageBuilder: (
-                    BuildContext context,
-                    Animation<double> animation,
-                    Animation<double> secondaryAnimation,
-                  ) {
-                    return AnimatedBuilder(
-                      animation: animation,
-                      builder: (context, child) {
-                        return Opacity(
-                          opacity: opacityCurve.transform(animation.value),
-                          child: _buildPage(context, imageName, description),
-                        );
-                      },
-                    );
-                  },
-                ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ChoiceCard(choice: choice),
               );
-            },
+            }).toList(),
           ),
         ),
       ),
     );
   }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'CAR', icon: Icons.directions_car),
+  const Choice(title: 'BICYCLE', icon: Icons.directions_bike),
+  const Choice(title: 'BOAT', icon: Icons.directions_boat),
+  const Choice(title: 'BUS', icon: Icons.directions_bus),
+  const Choice(title: 'TRAIN', icon: Icons.directions_railway),
+  const Choice(title: 'WALK', icon: Icons.directions_walk),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
 
   @override
   Widget build(BuildContext context) {
-    timeDilation = 5.0;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Radial Transition Demo'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(32),
-        alignment: FractionalOffset.bottomLeft,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            _buildHero(
-                context,
-                'https://shopstatic.vivo.com.cn/vivoshop/commodity/10/10006210_1610522662214_750x750.png.webp',
-                'vivo X60 pro+'),
-            _buildHero(
-                context,
-                'https://shopstatic.vivo.com.cn/vivoshop/commodity/10/10006210_1610522661982_750x750.png.webp',
-                '正面'),
-            _buildHero(
-                context,
-                'https://shopstatic.vivo.com.cn/vivoshop/commodity/10/10006210_1610522662754_750x750.png.webp',
-                '素皮'),
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
           ],
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(TabbedAppBarSample());
 }
